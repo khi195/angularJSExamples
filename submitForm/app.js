7c637b1a9d61c2aafc8c3dd75a0a6e8cdedcb4bd
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-10-28 23:31:22 +0100

    Button to delete the last element in the array

--- submitForm/app.js
+++ submitForm/app.js
@@ -6,13 +6,24 @@ app.controller('submit', function($scope){
 // define a array for storing the values
 
 $scope.submitArray = [];
-
 $scope.submit = function(){
 
 $scope.submitArray.push(this.submitMessage);
-console.log($scope.submitArray)
 
 }
+
+
+$scope.delete = function(){
+
+// element to delete
+var elementPosition = $scope.submitArray.length - 1;
+
+// delete the last element 
+$scope.submitArray.splice(elementPosition, 1);
+
+//
+console.log($scope.submitArray);
+};
 
 
 
