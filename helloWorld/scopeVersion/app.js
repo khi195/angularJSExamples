f9f8fb0342878312218bc92ff1cf7ae02d50be10
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-10-28 18:59:16 +0100

    intial commit for the hello world examples

---
+++ helloWorld/scopeVersion/app.js
@@ -0,0 +1,11 @@
+var app = angular. module('helloWorldExample', []);
+
+app.controller('helloWorld', function($scope){
+
+$scope.message = "Hello world";
+
+
+})
+
+
+
---
+++ helloWorld/thisVersion/app.js
@@ -0,0 +1,11 @@
+var app = angular. module('helloWorldExample', []);
+
+app.controller('helloWorld', function(){
+
+this.message = "Hello world";
+
+
+})
+
+
+
